bedf72eae73e2637868bce01f57b75230e12eaef
Mauriverti da Silva Junior <maurivertijr@gmail.com>
2018-07-29 23:16:28 -0300

    Add Recipe list with static data; Improve design of recipe detail

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'recipe-list',
-  templateUrl: './recipe-list.component.pug'
+  templateUrl: './recipe-list.component.pug',
+  styleUrls: ['recipe-list.component.css']
 })
-export class RecipeListComponent {}
+export class RecipeListComponent {
+
+  recipes: Recipe[] = [
+    new Recipe('First Recipe', 'Just a Recipe', 'http://kathysvegankitchen.com/wp-content/uploads/2017/03/33458820656_bb5ab18c77_o.jpg'),
+    new Recipe('First Recipe', 'Just a Recipe', 'http://kathysvegankitchen.com/wp-content/uploads/2017/03/33458820656_bb5ab18c77_o.jpg')
+  ];
+}
--- src/modules/angular-material.module.ts
+++ src/modules/angular-material.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import * as Mat from '@angular/material';
 
 @NgModule({
-  exports: [BrowserAnimationsModule, NoopAnimationsModule, Mat.MatIconModule, Mat.MatInputModule]
+  exports: [BrowserAnimationsModule, NoopAnimationsModule, Mat.MatIconModule, Mat.MatInputModule,
+    Mat.MatCardModule, Mat.MatButtonModule, Mat.MatDividerModule, Mat.MatTooltipModule]
 })
 export class AngularMaterialModule {}
